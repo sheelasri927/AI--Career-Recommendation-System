5d17c311e96353dede4fb6a430f4e392ec568dab
sheelasri927 <sheelasri678@gmail.com>
2026-08-18 21:41:02 +0530

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const form = document.querySelector("form");
+    const button = document.querySelector("button");
+
+    if (form && button) {
+        form.addEventListener("submit", function () {
+            button.innerHTML = "🤖 Analyzing...";
+            button.disabled = true;
+        });
+    }
+
+});
